2ed8a38e370bba7afa7a3be1dc9dff969c64dc24
kamal <klpradhan89@gmail.com>
2023-05-07 01:30:23 +0530

    launch setting added for Edge dve ttols debug in vscode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'AngularCrudFrontEnd1';
+
+  constructor()
+  {
+
+  }
+  ngOnInit(): void {
+   alert("Hi kamal");
+  }
+  
 }
