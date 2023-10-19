b8d56bf09ea7c07b8f7ae5b3bf7059d93fce5900
kamal <klpradhan89@gmail.com>
2023-10-19 21:17:03 +0530

    retry(4) rxjs operator called to call api 4 time If 1st time faIled

--- src/app/Interceptors/headerinterceptor.service.ts
+++ src/app/Interceptors/headerinterceptor.service.ts
@@ -1,6 +1,6 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, retry } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +9,6 @@ export class HeaderinterceptorService implements HttpInterceptor {
 
   constructor() { 
     debugger;
-
   }
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     req=req.clone({
@@ -17,6 +16,12 @@ export class HeaderinterceptorService implements HttpInterceptor {
         'Content-Type':'application/json'
       }
     });
-   return next.handle(req);
+   //return next.handle(req);
+   
+   /*retry operator is a rxjs operator and if the api calling is failing 1st time 
+   it will try another 4 time to call is */
+   return next.handle(req).pipe(
+    retry(4)
+   );
   }
 }
